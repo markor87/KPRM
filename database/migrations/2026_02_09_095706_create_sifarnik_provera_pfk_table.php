b9ddc7c5ee4f1d0b793f026c318e5098786bc931
marko.radovanovic <mradovanovic@msn.com>
2026-02-09 11:45:21 +0100

    brisanje stare migracije i jedna nova migracija

--- database/migrations/2026_02_09_095706_create_sifarnik_provera_pfk_table.php
+++ database/migrations/2026_02_09_095706_create_sifarnik_provera_pfk_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('groups', function (Blueprint $table) {
+        Schema::create('sifarnik_provera_pfk', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('description')->nullable();
-            $table->timestamps();
+            $table->string('provera_pfk')->nullable();
         });
     }
 
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('sifarnik_provera_pfk');
     }
 };
