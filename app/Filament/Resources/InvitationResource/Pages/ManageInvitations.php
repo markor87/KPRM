7e1acba2dce3f81612dcc420525d710db4e2337d
marko.radovanovic <mradovanovic@msn.com>
2026-07-14 14:55:37 +0200

    Позивнице: лимит слања се троши по покушају
    Лимит од 20 на сат сада се броји пре слања (неуспео SMTP такође
    троши лимит) да се сервер за пошту не би могао злоупотребити за
    неограничене покушаје. Провера лимита се ради и унутар петље, па
    једна велика група адреса не може да заобиђе лимит одједном;
    прескочене позивнице се пријављују кориснику.

--- app/Filament/Resources/InvitationResource/Pages/ManageInvitations.php
+++ app/Filament/Resources/InvitationResource/Pages/ManageInvitations.php
@@ -24,10 +24,13 @@ class ManageInvitations extends ManageRecords
                 ->label('Пошаљи позивнице')
                 ->icon('heroicon-o-paper-airplane')
                 ->using(function (array $data) {
-                    // Rate limiting: max 20 pozivnica po korisniku na sat
+                    // Rate limiting: max 20 pozivnica po korisniku na sat.
+                    // Limit se troši po POKUŠAJU (ne po uspešnom slanju) da neispravan
+                    // SMTP ne bi mogao da se zloupotrebi za neograničene pokušaje slanja.
                     $rateLimitKey = 'invitations:' . auth()->id();
+                    $maxPerHour = 20;
 
-                    if (RateLimiter::tooManyAttempts($rateLimitKey, 20)) {
+                    if (RateLimiter::tooManyAttempts($rateLimitKey, $maxPerHour)) {
                         $seconds = RateLimiter::availableIn($rateLimitKey);
                         $minutes = ceil($seconds / 60);
 
@@ -46,8 +49,18 @@ class ManageInvitations extends ManageRecords
 
                     $invitationCount = 0;
                     $failedEmails = [];
+                    $skippedForLimit = 0;
 
                     foreach ($emails as $email) {
+                        // Provera i unutar petlje da jedan veliki batch ne zaobiđe limit odjednom
+                        if (RateLimiter::tooManyAttempts($rateLimitKey, $maxPerHour)) {
+                            $skippedForLimit++;
+                            continue;
+                        }
+
+                        // Broji pokušaj pre slanja - neuspeh takođe troši limit
+                        RateLimiter::hit($rateLimitKey, 3600);
+
                         $invitation = Invitation::create([
                             'email'      => $email,
                             'token'      => Str::random(64),
@@ -58,7 +71,6 @@ class ManageInvitations extends ManageRecords
 
                         try {
                             Mail::to($invitation->email)->send(new InvitationMail($invitation));
-                            RateLimiter::hit($rateLimitKey, 3600);
                             $invitationCount++;
                         } catch (\Exception $e) {
                             $invitation->delete();
@@ -79,6 +91,15 @@ class ManageInvitations extends ManageRecords
                             ->title('Грешка при слању')
                             ->body('Слање није успело за: ' . implode(', ', $failedEmails))
                             ->danger()
+                            ->persistent()
+                            ->send();
+                    }
+
+                    if ($skippedForLimit > 0) {
+                        Notification::make()
+                            ->title('Лимит слања достигнут')
+                            ->body("{$skippedForLimit} позивница није послато јер је достигнут лимит од {$maxPerHour} на сат. Покушајте поново касније.")
+                            ->warning()
                             ->persistent()
                             ->send();
                     }
